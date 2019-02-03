02434135d05f2a2013fdd2908e59f891209f1586
Mike Russell <mike.russell@gov.ab.ca>
2019-02-03 14:18:58 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -26,7 +26,7 @@ shinyUI(pageWithSidebar(
  #             accept='.csv'),
     sliderInput("AEPBudget", "Step 1: Choose the RELM funding amount (thousands)", min=0, max=3000, value=684, step = 5),
     sliderInput("EMSDBudget", "Step 2: Choose the EMSD funding amount (thousands)", min=0, max=1250, value=325, step = 5),
-    sliderInput("ForecastYr", "Step 3: What year would you like to forecast to?", min=2019, max=2050, value=2040, step=1),
+    sliderInput("ForecastYr", "Step 3: What year would you like to forecast to?", min=2020, max=2050, value=2040, step=1),
     checkboxInput("TopUp","Check if Ops funds will be used to top-up WMUs designated for EMSD funding", FALSE)
 
   ),
